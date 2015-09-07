f922794831e887e65d28cc9a1fe4311b82b8d17e
Antony Savio <antony@spoonity.com>
2015-09-06 20:23:23 -0400

    adding Jurassic Park node app

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-/**
- * Created by Antony on 9/6/2015.
- */
+
+
+var express=require('express');
+
+var app=express();
+
+app.get('/',function(req,res){
+res.send("Jurassic Park eco-system monitor");
+
+});
+
+
+app.listen(process.env.PORT || 5000);
+
+module.exports=app ;
--- test/test.js
+++ test/test.js
@@ -1,3 +1,13 @@
 /**
  * Created by Antony on 9/6/2015.
  */
+
+var request=require('supertest');
+
+var app=require('../app.js');
+
+describe('GET /',function(){
+   it('provides application name as response',function(done){
+      request(app).get('/').expect('Jurassic Park eco-system monitor',done);
+   });
+});
